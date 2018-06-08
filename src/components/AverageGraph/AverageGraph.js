a5b7491383c9c587b8eab5c948db75abfaed6680
Greg Orvik <gvorvik@gmail.com>
2018-06-08 15:34:31 -0500

    center the graph of average scores and display accurate data on dom

--- src/components/AverageGraph/AverageGraph.js
+++ src/components/AverageGraph/AverageGraph.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
+import { Bar } from 'react-chartjs-2';
+
 class AverageGraph extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            data: []
+            chartData: {}
         }
     }
 
@@ -15,25 +17,63 @@ class AverageGraph extends Component {
         this.getDataForGraph();
     }
 
+    static defaultProps = {
+        displayTitle: true,
+        displayLegend: true,
+        legendPosition: 'bottom',
+    }
+
     getDataForGraph = () => {
         axios({
             method: 'GET',
             url: '/api/students/average',
         })
-        .then(result => {
-            console.log(result.data);
-            this.setState({
-                data: result.data
+            .then(response => {
+                this.setState({
+                    chartData: {
+                        labels: response.data.map((score) => {
+                            let grade = score.grade;
+                            return grade;
+                        }),
+                        datasets: [
+                            {
+                                label: 'Average By Grade',
+                                data: response.data.map(score => score.avg),
+                                backgroundColor: 'rgba(230, 126, 34, 0.6)'
+                            }
+                        ]
+                    }
+                });
             })
-        })
-        .catch(err => console.log(err));
+            .catch(err => console.log(err));
     }
 
     render() {
 
         return (
-            <div>
-                <h1>Hello Average Graph</h1>
+            <div style={{width: '600px', height: '450px', margin: "0 auto"}}>
+                <Bar 
+                data={this.state.chartData}
+                options={{
+                    title: {
+                        display: this.props.displayTitle,
+                        text: 'Average By Grade',
+                        fontSize: 25,
+                    },
+                    legend: {
+                        display: this.props.displayLegend,
+                        position: this.props.legendPosition
+                    },
+                    maintainAspectRatio: false,
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                            }
+                        }]
+                    }
+                }}
+            />
             </div>
         );
     }
