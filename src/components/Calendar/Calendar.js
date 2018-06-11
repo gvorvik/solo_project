76bc1e74481ca3931c951328592ade91f2f96e5c
Greg Orvik <gvorvik@gmail.com>
2018-06-11 09:51:23 -0500

    use material ui to style student dropdown

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -2,20 +2,42 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Nav from '../Nav/Nav';
+import { USER_ACTIONS } from '../../redux/actions/userActions';
+
+const mapStateToProps = state => ({
+    user: state.user,
+});
 
 class Calendar extends Component {
+    componentDidMount() {
+        this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
+    }
+
+    componentDidUpdate() {
+        if (!this.props.user.isLoading && this.props.user.userName === null) {
+            this.props.history.push('home');
+        }
+    }
 
     render() {
+
+        let content = null;
+
+        if (this.props.user.userName) {
+            content = (
+                <div id="calendarDiv">
+                    <iframe title="GregCalendar" src="https://calendar.google.com/calendar/embed?src=gvorvik%40gmail.com&ctz=America%2FChicago" style={{ width: "800px", height: "600px", frameborder: "0", scrolling: "no" }}></iframe>
+                </div>
+            );
+        }
 
         return (
             <div>
                 <Nav />
-                <div id="calendarDiv">
-                    <iframe title="GregCalendar" src="https://calendar.google.com/calendar/embed?src=gvorvik%40gmail.com&ctz=America%2FChicago" style={{ width: "800px", height: "600px", frameborder: "0", scrolling: "no"}}></iframe>
-                </div>
+                {content}
             </div>
         );
     }
 }
 
-export default connect()(Calendar);
+export default connect(mapStateToProps)(Calendar);
--- src/components/StudentDropdown/StudentDropdown.js
+++ src/components/StudentDropdown/StudentDropdown.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select'
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import { STUDENT_ACTIONS } from '../../redux/actions/studentActions';
 
@@ -27,45 +31,55 @@ class StudentDropdown extends Component {
             method: 'GET',
             url: `/api/students/studentinfo/${this.state.studentID}`
         })
-        .then((response) => {
-            const student = response.data[0];
-            const action = {
-                type: STUDENT_ACTIONS.SET_STUDENT_ID_IN_STORE,
-                payload: {
-                  id: student.id,
-                  firstName: student.first_name,
-                  lastName: student.last_name
-                },
-              }
-            this.props.dispatch(action);
-            this.props.getStudentScores(student.id);
-        })
-        .catch(err => console.log(err));
+            .then((response) => {
+                const student = response.data[0];
+                const action = {
+                    type: STUDENT_ACTIONS.SET_STUDENT_ID_IN_STORE,
+                    payload: {
+                        id: student.id,
+                        firstName: student.first_name,
+                        lastName: student.last_name
+                    },
+                }
+                this.props.dispatch(action);
+                this.props.getStudentScores(student.id);
+            })
+            .catch(err => console.log(err));
 
     }
 
 
     handleChange = (event) => {
         this.setState({
-                studentID: Number(event.target.value),
+            studentID: Number(event.target.value),
         })
     }
 
     render() {
 
         let students = this.props.reduxState.student.studentReducer.map((student) => {
-            return <option key={student.id} value={student.id}>
-                        {student.first_name} {student.last_name}
-                   </option>
+            // return <option key={student.id} value={student.id}>
+            //     {student.first_name} {student.last_name}
+            // </option>
+            return <MenuItem key={student.id} value={student.id}>{student.first_name} {student.last_name}</MenuItem>
         })
 
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <select onChange={this.handleChange}>
-                        {students}
-                    </select>
-                    <input type="submit"/>
+                <form onSubmit={this.handleSubmit} style={{ textAlign: "center" }}>
+                    <FormControl style={{width: "200px"}}>
+                        <InputLabel>Student</InputLabel>
+                        <Select
+                            value={this.state.studentID}
+                            onChange={this.handleChange}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            {students}
+                        </Select>
+                    </FormControl>
+                    <input type="submit" />
                 </form>
             </div>
         );
