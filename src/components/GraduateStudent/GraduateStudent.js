05bc8fd6919e0c7053c57c581eb5be29c397e066
Greg Orvik <gvorvik@gmail.com>
2018-06-13 17:27:03 -0500

    bring in btn from material ui for graduating a student

--- src/components/GraduateStudent/GraduateStudent.js
+++ src/components/GraduateStudent/GraduateStudent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 class Graduate extends Component {
 
     render() {
         return (
-            <div>
-                <button onClick={() => {this.props.graduateStudent(this.props.studentID)}}>Push Me</button>
+            <div id="graduateStudent">
+                <Button variant="raised" color="primary" onClick={() => {this.props.graduateStudent(this.props.studentID)}}>Graduate Student</Button>
             </div>
         );
     }
