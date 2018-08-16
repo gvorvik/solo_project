69795c1c7b99a078f6f1afb619a582aa4c954162
Greg Orvik <gvorvik@gmail.com>
2018-08-16 11:18:47 -0500

    make graduate button component functional

--- src/components/GraduateStudent/GraduateStudent.js
+++ src/components/GraduateStudent/GraduateStudent.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 
-class Graduate extends Component {
-
-    render() {
-        return (
-            <div id="graduateStudent">
-                <Button variant="raised" color="primary" onClick={() => {this.props.graduateStudent(this.props.studentID)}}>Graduate Student</Button>
-            </div>
-        );
-    }
+const Graduate = (props) => {
+    return (
+        <div id="graduateStudent">
+            <Button variant="raised" color="primary" onClick={() => {props.graduateStudent(props.studentID)}}>Graduate Student</Button>
+        </div>
+    );
 }
 
 export default Graduate;
